5c6b63cba2122f1ed34c4df35c02c14d7a17cc11
bibisixtynine <ilboued@me.com>
2023-01-25 18:46:55 +0100

    okokok

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import './styles.css';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
+// viewport meta tag must be in _app.js (https://nextjs.org/docs/messages/no-document-viewport-meta)
+
+function App({ Component, pageProps }) {
+  <Head>
+    <meta
+      name="viewport"
+      content="viewport-fit=cover,user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1"
+    />
+  </Head>
   return <Component {...pageProps} />
 }
-
-export default MyApp
+export default App;
