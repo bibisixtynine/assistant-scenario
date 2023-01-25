4600a961729595df008d1842bf2a6f059b4aa6d1
bibisixtynine <ilboued@me.com>
2023-01-25 18:36:19 +0100

    okok

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,110 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Image from 'next/image';
 
-export default function Home() {
+import coffeeLogo from '../assets/coffee-logo.png'
+
+import { useState } from 'react';
+
+import {ANSI} from '../utils/ansi_colors.js';
+
+console.log(ANSI.GREEN + "\n*--- index.js (re)STARTED ---*" + ANSI.RESET);
+
+const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  //const callGenerateEndpoint = async () => {
+  const callGenerateEndpoint = async () => {
+      setIsGenerating(true);
+
+    /*
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });*/
+
+    //const data = await response.json();
+    const data = {text:'OH YES'}
+
+    const { output } = data;
+
+    setApiOutput(`${ userInput }${output.text}`);
+    setIsGenerating(false);
+  }
+
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+
   return (
-    <div className={styles.container}>
+    <div className="root">
       <Head>
-        <title>Create Next App</title>
+        <title>Créateur de scénario</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>Demandez votre scenario !</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>Donnez moi quelques éléments dans le cadre ci-dessous (qui peut se limiter à par exemple : la cigale et la fourmi), et je m'occupe de rédiger votre scénario !</h2>
+            <h2>Ensuite, appuyez sur 'GO!', et accordez moi entre 10 et 20s pour vous répondre!</h2>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a href="https://next.new" target="_blank" rel="noopener noreferrer">
-          Created with&nbsp;<b>next.new</b>&nbsp;⚡️
+        <div className="prompt-container">
+          <textarea
+            placeholder="Commencez par remplacer ce message en cliquant / touchant dans le cadre puis écrivez un résumé en quelques lignes, ou juste quelques éléments clés de votre histoire. "
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? <span className="loader"></span> : <p>GO!</p>}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>L'histoire que j'ai rédigée pour vous :</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+          <div className="contact">
+            <h2>Contact: <a href="mailto:profpepite@proton.me" style={{ color: 'gray' }}>profpepite@proton.me</a></h2>
+          </div>
+        </div>
+      </div>
+      <div className="badge-container grow">
+        <a
+          href="https://donate.stripe.com/00g15qbu32to4mY8ww"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <Image src={coffeeLogo} alt="coffee logo" />
+            <p>offrir un café </p>
+          </div>
         </a>
-      </footer>
+      </div>
     </div>
   );
-}
+};
+
+
+
+export default Home;
