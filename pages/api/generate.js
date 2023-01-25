601efb04e53028310961fdc4bed007bf880c3003
bibisixtynine <ilboued@me.com>
2023-01-25 17:21:31 +0100

    supp dotenv

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -48,11 +48,13 @@ export function UUID() {
 // FIREBASE 8
 //
 // 1. init
+const FIREBASE_DATABASE_URL = 'https://scenario-assistant-default-rtdb.europe-west1.firebasedatabase.app/'
+
 const firebaseConfig = {
   databaseURL:
-    process.env.FIREBASE_DATABASE_URL,
+  FIREBASE_DATABASE_URL,
 };
-console.log(ANSI.BLUE + '\nFIREBASE_DATABASE_URL : ' + process.env.FIREBASE_DATABASE_URL + ANSI.RESET)
+console.log(ANSI.BLUE + '\nFIREBASE_DATABASE_URL : ' + FIREBASE_DATABASE_URL + ANSI.RESET)
 //
 // 2. Initialize Firebase
 const apptiti = firebase.initializeApp(firebaseConfig);
@@ -91,20 +93,35 @@ newLogData.on('value', (snapshot) => {
 
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: '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',
 });
 
 const openai = new OpenAIApi(configuration);
 
 const generateAction = async (req, res) => {
+  const OPENAI_REQUEST_DESCRIPTION_FR = `Ecriture d'un scénario de dessin animé en 7 actes.
+  Le dernier acte doit permettre de bien comprendre la morale de l'histoire.
+  Donner un titre en lettres majuscules à ce scénario.
+  Si les noms des personnages ne sont pas donnés, les remplacer par des noms génériques (ex : le héros, le méchant, le père, la mère, le frère, la sœur, le grand-père, la grand-mère, le chien, le chat, le poisson rouge, etc...).
+  Pour chaque acte :
+  (1) Donner une description précise et imagée de l'environnement, encadrée de caractères #. 
+  (2) Ajouter ensuite un échange entre les personnages. 
+  (3) Ajouter entre paranthèses pour chacun des dialogues une description de l'état émotionnel du personnage, juste après son nom.
+  (4) Chaque dialogue de chacun des personnage doit apparaitre sur une nouvelle ligne. 
+  (5) Chaque échange doit comporter un minimum de 10 dialogues.
+  (6) Ajouter un titre en lettres capitales.
+  
+  Synopsis :`
 
-  const firstPrompt = `${process.env.OPENAI_REQUEST_DESCRIPTION_FR}${req.body.userInput}`;
+  const firstPrompt = `${OPENAI_REQUEST_DESCRIPTION_FR}${req.body.userInput}`;
 
   console.log(ANSI.CYAN + '\n' + firstPrompt + ANSI.GREEN + '...' + ANSI.RESET)
 
+
+
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${process.env.OPENAI_REQUEST_DESCRIPTION_FR}${req.body.userInput}`,
+    prompt: `${OPENAI_REQUEST_DESCRIPTION_FR}${req.body.userInput}`,
     temperature: 0.7,
     max_tokens: 1900,
   });
@@ -112,7 +129,7 @@ const generateAction = async (req, res) => {
   const basePromptOutput = baseCompletion.data.choices.pop();
 
   writeData('openai-scenario-serverside', UUID(), {
-    brief: process.env.OPENAI_REQUEST_DESCRIPTION_FR,
+    brief: OPENAI_REQUEST_DESCRIPTION_FR,
     prompt: req.body.userInput,
     answer: `${req.body.userInput}${basePromptOutput.text}`
   })
